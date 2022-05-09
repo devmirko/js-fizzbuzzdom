9e678f1b75902850f6516e64d1dc55e2a3fd1fb0
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-09 09:52:13 +0200

    fine esercizio( da modificare stile)

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// ciclo for per stampare numeri da 1 a 100
+const container = document.querySelector("div.box")
+
+for ( let i = 1; i <= 100; i++) {
+    if(( i % 3 === 0) && ( i%5 === 0 )) {
+
+        const element = '<div class="square fizz_buzz"> FizzBuzz </div> ';
+        container.innerHTML += element;
+   
+} else if( i % 3 === 0) {
+
+    const element = '<div class="square fizz"> FizzBuzz </div> ';
+    container.innerHTML += element;
+
+}  else if( i % 5 === 0) {
+
+    const element = '<div class="square buzz"> Buzz </div> ';
+    container.innerHTML += element;
+
+} else {
+    const element = '<div class="square other"> ${1} </div>';
+    container.innerHTML += element;
+}
+
+}
