66bba5ed898d3431e1ed81b7d7ae1da3fd982236
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-09 12:56:04 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -4,22 +4,23 @@ const container = document.querySelector("div.box")
 for ( let i = 1; i <= 100; i++) {
     if(( i % 3 === 0) && ( i%5 === 0 )) {
 
-        const element = '<div class="square fizz_buzz"> FizzBuzz </div> ';
+        const element = '<div class="square fizz_buzz "> FizzBuzz </div>';
         container.innerHTML += element;
    
 } else if( i % 3 === 0) {
 
-    const element = '<div class="square fizz"> FizzBuzz </div> ';
+    const element = '<div class="square fizz"> FizzBuzz </div>';
     container.innerHTML += element;
 
 }  else if( i % 5 === 0) {
 
-    const element = '<div class="square buzz"> Buzz </div> ';
+    const element = '<div class="square buzz"> Buzz </div>';
     container.innerHTML += element;
 
 } else {
-    const element = '<div class="square other"> ${1} </div>';
+    const element = `<div class="square other">${i}</div>`;
     container.innerHTML += element;
 }
 
 }
+
